724236f202c876ef3714f488aa8043e7f72f0fdc
Taskim <tony@evaneos.com>
2019-08-23 10:50:55 +0200

    Updates

---
+++ precache-manifest.453d5542b6a0a177885572de7e0a2639.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67aa98e94eaf24dfd56dc82ca47086bf",
+    "url": "/integration-testing-workshop/index.html"
+  },
+  {
+    "revision": "89c55ab746b7415fd36c",
+    "url": "/integration-testing-workshop/static/css/main.4bb74d97.chunk.css"
+  },
+  {
+    "revision": "cb663949948093274eed",
+    "url": "/integration-testing-workshop/static/js/2.de3906b9.chunk.js"
+  },
+  {
+    "revision": "89c55ab746b7415fd36c",
+    "url": "/integration-testing-workshop/static/js/main.b995d42b.chunk.js"
+  },
+  {
+    "revision": "54856851aa956c3a563f",
+    "url": "/integration-testing-workshop/static/js/runtime~main.b324c6da.js"
+  }
+]);
